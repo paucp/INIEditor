00631191e30fedfa1eb3b02965c9e0fd79852118
paucp <20616716+paucp@users.noreply.github.com>
2019-08-03 12:19:36 +0200

    FileStructure

--- INIEditor/Ini.cs
+++ INIEditor/Ini.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace INIEditor
 {
     public struct IniGroup
     {
         public string Name;
-        public Dictionary<string, string> IniKeys;
+        public IDictionary<string, string> IniKeys;
     }    
     public class Ini
     {        
@@ -18,23 +19,35 @@ namespace INIEditor
         {
             IniLines = RemoveInvalidLines(IniLines);
             List<IniGroup> IniGroups = new List<IniGroup>();
-            int GroupStartLine = FindGroupLine(IniLines, 0);
+            int GroupStartLine = 0;
             int GroupEndLine = FindGroupLine(IniLines, 1);
-
+            while(GroupStartLine != -1) {
+                IniGroups.Add(ParseGroup(IniLines, GroupStartLine, GroupEndLine));
+                GroupStartLine = FindGroupLine(IniLines, GroupEndLine);
+                GroupEndLine = FindGroupLine(IniLines, GroupStartLine+1);
+            }
+            return IniGroups;
         }
-        private IniGroup ParseGroup(string[] GroupText, int StartLine, int EndLine)
+        private IniGroup ParseGroup(string[] IniLines, int StartLine, int LastLine)
         {
-            IniGroup Group;
+            if (LastLine == -1) LastLine = IniLines.Length;
+            IniGroup Group = new IniGroup();
             Group.IniKeys = new Dictionary<string, string>();
-
+            Group.Name = GetGroupName(IniLines[StartLine]);
+            for (int i = StartLine+1; i < LastLine - StartLine; ++i)
+                Group.IniKeys.Add(GetLineKeyAndValue(IniLines[i]));
+            return Group;
         }
-        public string GetIniText()
+        string GetGroupName(string Line)
+           => Line.Substring(1, Line.IndexOf("]") - 1);
+        KeyValuePair<string, string> GetLineKeyAndValue(string Line)
         {
-            return "";
-        }
-        
+            string[] Split = Line.Split('=');
+            return new KeyValuePair<string, string>(Split[0], Split[1]);
+        }           
         int FindGroupLine(string[] Lines, int StartLine)
         {
+            if (StartLine >= Lines.Length) return -1;
             for (int i = StartLine; i < Lines.Length; ++i)
                 if (Lines[i].StartsWith("[") && Lines[i].EndsWith("]"))
                     return i;
@@ -47,9 +60,27 @@ namespace INIEditor
                 if (!string.IsNullOrEmpty(Lines[i]) && !string.IsNullOrWhiteSpace(Lines[i])
                     && !Lines[i].StartsWith("#") && !Lines[i].StartsWith("//") && !Lines[i].StartsWith("*"))
                 {
-                    FormattedLines[i] = Lines[i];
+                    FormattedLines[i] = RemoveWhiteSpaces(Lines[i]);
                 }
             return FormattedLines;
+        }
+        string RemoveWhiteSpaces(string Line)
+        {
+            StringBuilder CleanedLine = new StringBuilder();
+            for (int i = 0; i < Line.Length; ++i)
+                if (Line[i] != ' ') CleanedLine.Append(Line[i]);
+            return CleanedLine.ToString();
+        }
+        public string GetIniText()
+        {
+            StringBuilder IniText = new StringBuilder();
+            for (int i = 0; i < Groups.Count; ++i)
+            {
+                IniText.AppendLine("[" + Groups[i].Name + "]");
+                foreach (KeyValuePair<string, string> Key in Groups[i].IniKeys)
+                    IniText.AppendLine(Key.Key + "=" + Key.Value);
+            }
+            return IniText.ToString();
         }
     }
 }
--- INIEditor/Main.cs
+++ INIEditor/Main.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace INIEditor
 {
     public partial class Main : Form
     {
+        private Ini Ini;
+        private IniIO IniIO;
         public Main()
         {
             InitializeComponent();
+        }
+        private void AddKey()
+        {
+
         }
     }
 }
